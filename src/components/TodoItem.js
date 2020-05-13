0ba929178731dbb2364c2b95baad93048e2dfc80
Mirwais <mirwais@vitechparts.com>
2020-05-13 14:52:17 +0200

    add checkbox to TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,9 +4,11 @@ export default class TodoItem extends Component {
     render() {
         const { title,handleDelete,handleEdit } = this.props;
         return (
+
             <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
                 <h6>{title}</h6>
                 <div className="todo-icon">
+                    <input type="checkbox" />
                     <span className="mx-2 text-success" onClick={handleEdit} >
                         <i className="fas fa-pen"></i>
                     </span>
